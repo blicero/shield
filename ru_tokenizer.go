6b02025761ad99db30cc0c1ce92206b72783c103
Alexander Garin <garin1221w@gmail.com>
2016-08-27 23:46:21 +0500

    Add Rus tokenizer

--- ru_tokenizer.go
+++ ru_tokenizer.go
@@ -2,6 +2,7 @@ package shield
 
 import (	
 	"strconv"
+    "github.com/legion-zver/shield/porterstemmers"
 )
 
 
@@ -15,10 +16,20 @@ func NewRussianTokenizer() Tokenizer {
 
 func (t *ruTokenizer) Tokenize(text string) (words map[string]int64) {
     words = make(map[string]int64)
+    pvStr := ""
+    rusPS := porterstemmers.RussianPorterStemmer{}
     for _, w := range splitTokenRx.Split(text, -1) {
         if len(w) > 2 {
             if _, err := strconv.Atoi(w); err != nil {
-
+                stem := rusPS.StemString(w)
+                if len(stem) > 2 {
+                    words[stem]++
+                    if "" != pvStr {
+                        bgWord := pvStr + "_" + stem							
+                        words[bgWord]++
+                    }
+                    pvStr = stem
+                }
             }
         }
     }
